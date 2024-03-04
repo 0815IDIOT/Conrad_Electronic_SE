3c52e7e7083294b4d2ea7f4bb61577716a77903f
Florian Franke <mail@florian-franke.eu>
2024-03-04 14:48:08 +0100

    add some details into the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install python3 sqlite3
+
 if [ -f resources/data.db ]; then
     echo "[*] deleting old DB" 
     rm resources/data.db
